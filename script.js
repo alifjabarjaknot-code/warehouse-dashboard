d89a0fd080af61e75d868c469e4dc8eca90f10f4
alifjabarjaknot-code <alifjabar.jaknot@gmail.com>
2026-07-27 03:27:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Warehouse Dashboard Loaded");
+const ctx = document.getElementById('orderChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+        datasets: [{
+            label: 'Order',
+            data: [320, 410, 380, 520, 610, 560],
+            borderWidth: 3,
+            tension: 0.4
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true
+            }
+        }
+    }
+});
